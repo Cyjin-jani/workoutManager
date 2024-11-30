ec63f65006a7552582d244d631f9d145113a61a4
yj Cho <57857367+Cyjin-jani@users.noreply.github.com>
2024-11-30 23:17:20 +0900

    Wm 16 (#1)
    * chore: tanstack query 패키지 추가
    
    * feat: Providers 컴포넌트 추가
    
    * feat: layout에 Providers 적용
    
    ---------
    
    Co-authored-by: Yeonjin <111411106+yjdev91@users.noreply.github.com>

---
+++ src/app/Providers.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { QueryClientProvider } from '@tanstack/react-query';
+import type * as React from 'react';
+
+import { QueryClient, isServer } from '@tanstack/react-query';
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+      },
+      mutations: {
+        retry: false,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
