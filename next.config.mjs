51bb7ee9c1f58b766b1ac665bf86a8935597dc19
Cyjin-jani <jinjanic91@gmail.com>
2024-12-14 16:26:02 +0900

    chore: ts 대신 mjs로 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-import type { NextConfig } from 'next';
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform({});
 }
 
-const nextConfig: NextConfig = {
+// https://github.com/vercel/next.js/issues/67765
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
 };
 
