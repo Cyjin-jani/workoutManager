27361db6f6ef0b423b1ffea1d173f93ac42bc541
Cyjin-jani <jinjanic91@gmail.com>
2024-12-16 22:36:21 +0900

    refactor: queryOptions를 사용하도록 useUsers 훅 수정

--- src/app/hooks/queries/useUsers.tsx
+++ src/app/hooks/queries/useUsers.tsx
@@ -1,4 +1,6 @@
-import { useQuery, QueryFunction } from '@tanstack/react-query';
+import type { UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
+import { queryOptions, useQuery } from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
 import axios from 'axios';
 
 interface User {
@@ -7,13 +9,21 @@ interface User {
   email: string;
 }
 
-const fetchUsers: QueryFunction<User[]> = async () => {
+const fetchUsers = async (): Promise<User[]> => {
   const { data } = await axios.get<User[]>('/api/users');
   return data;
 };
 
-const useUsers = () => {
-  return useQuery<User[], Error>({ queryKey: ['users'], queryFn: fetchUsers });
+function usersOptions(options?: Partial<UseQueryOptions<User[], AxiosError>>) {
+  return queryOptions({
+    queryKey: ['users'],
+    queryFn: fetchUsers,
+    ...options,
+  });
+}
+
+const useUsers = (): UseQueryResult<User[], AxiosError> => {
+  return useQuery<User[], AxiosError>(usersOptions());
 };
 
 export default useUsers;
