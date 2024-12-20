04d427f9662b9e4d36a43b4083683de362fd3b2f
Cyjin-jani <jinjanic91@gmail.com>
2024-12-20 23:52:45 +0900

    feat: user table init sql 수정

--- migrations/0001_create_user_table.sql
+++ migrations/0001_create_user_table.sql
@@ -2,7 +2,14 @@
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "email" TEXT NOT NULL,
-    "name" TEXT
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT,
+    "profileSrc" TEXT,
+    "age" INTEGER,
+    "height" FLOAT,
+    "weight" FLOAT,
+    "gender" TEXT
 );
 
 -- CreateIndex
