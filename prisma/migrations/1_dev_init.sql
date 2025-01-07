5cbbc4067a7cdc9b869071d8b3c4967930b63274
yj Cho <57857367+Cyjin-jani@users.noreply.github.com>
2025-01-07 23:25:22 +0900

    WM-22: [DB] 운동 계획(로그) 모델 설계 (#6)
    * feat: 운동 계획(로그) 스키마 모델 작성
    
    * feat: prisma 모델 스키마에 맞는 DB와 sql 추가
    
    * refactor: reps -> repeats로 수정
    
    * feat: date에서 unique 제거
    
    * fix: 올바른 relation 관계 설정

---
+++ prisma/migrations/1_dev_init.sql
@@ -0,0 +1,33 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "WorkoutLog" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "date" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "WorkoutLogDetail" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "logId" INTEGER NOT NULL,
+    "exerciseName" TEXT NOT NULL,
+    "weight" REAL NOT NULL,
+    "sets" INTEGER NOT NULL,
+    "repeats" INTEGER NOT NULL,
+    "restTimeSeconds" INTEGER NOT NULL,
+    CONSTRAINT "WorkoutLogDetail_logId_fkey" FOREIGN KEY ("logId") REFERENCES "WorkoutLog" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WorkoutLog_date_key" ON "WorkoutLog"("date");
