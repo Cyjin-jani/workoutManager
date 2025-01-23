e4870000bdcaaeafcbaa7d06287cb047d3cfb9d6
Cyjin-jani <jinjanic91@gmail.com>
2025-01-24 00:36:25 +0900

    feat: 메인 화면에 로그인 유무 확인 간단 로직 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+import { cookies } from 'next/headers';
 import Link from 'next/link';
+import { AUTH_ACCESS_TOKEN } from './login/constants';
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  const isLoggedIn = !!cookieStore.get(AUTH_ACCESS_TOKEN);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>메인 페이지</h1>
-        <Link href="/login">로그인 하러 가기</Link>
+        {isLoggedIn ? (
+          <div>
+            <p>로그인 된 유저의 화면입니다.</p>
+          </div>
+        ) : (
+          <Link href="/login" className="text-blue-500 hover:underline">
+            로그인하러 가기
+          </Link>
+        )}
       </main>
     </div>
   );
