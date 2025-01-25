ce17ffd1cf3088eec4a47ee9939d893195a52cc3
Cyjin-jani <jinjanic91@gmail.com>
2025-01-26 00:00:25 +0900

    refactor: import에 type 키워드 지정

--- src/app/hooks/queries/useUsers.ts
+++ src/app/hooks/queries/useUsers.ts
@@ -2,7 +2,7 @@ import type { UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
 import { queryOptions, useQuery } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 import axios from 'axios';
-import { User } from '@prisma/client';
+import type { User } from '@prisma/client';
 
 const fetchUsers = async (): Promise<User[]> => {
   const { data } = await axios.get<User[]>('/api/users');
