1e5e56101c0a4277ba78a9b48361fd4c993d7db3
Cyjin-jani <jinjanic91@gmail.com>
2025-01-29 17:21:42 +0900

    feat: 메인 페이지 로그인 검증 로직 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
-import { AUTH_ACCESS_TOKEN } from '@/app/api/auth/login/route';
-import { cookies } from 'next/headers';
 import Link from 'next/link';
+import type { User } from '@prisma/client';
+import { cookies } from 'next/headers';
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  const isLoggedIn = !!cookieStore.get(AUTH_ACCESS_TOKEN);
+  const user = await getAuthenticatedUser();
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>메인 페이지</h1>
-        {isLoggedIn ? (
+        {user ? (
           <div>
             <p>로그인 된 유저의 화면입니다.</p>
           </div>
@@ -22,4 +21,23 @@ export default async function Home() {
       </main>
     </div>
   );
+}
+
+async function getAuthenticatedUser(): Promise<User | null> {
+  try {
+    const cookieStore = await cookies();
+    const response = await fetch(`${process.env.API_BASE_URL}/api/auth/me`, {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+
+    if (!response.ok) {
+      return null;
+    }
+
+    return response.json();
+  } catch {
+    return null;
+  }
 }
