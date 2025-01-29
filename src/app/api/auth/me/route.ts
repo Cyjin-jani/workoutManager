3ac6f5743c1d98dc3d9b46944f8da391cb3746c5
Cyjin-jani <jinjanic91@gmail.com>
2025-01-29 17:20:26 +0900

    feat: auth/me api 추가

---
+++ src/app/api/auth/me/route.ts
@@ -0,0 +1,32 @@
+import { get } from '@/app/lib/cf';
+import { NextRequest, NextResponse } from 'next/server';
+import { verifyAccessToken } from '@/app/lib/auth';
+import { AUTH_ACCESS_TOKEN } from '../login/route';
+
+export const runtime = 'edge';
+
+export async function GET(request: NextRequest) {
+  const accessToken = request.cookies.get(AUTH_ACCESS_TOKEN)?.value;
+
+  if (!accessToken) {
+    return NextResponse.json(null, { status: 401 });
+  }
+
+  try {
+    const payload = await verifyAccessToken(accessToken);
+    const { db } = await get();
+    const user = await db.user.findUnique({
+      where: {
+        id: payload.userId as number,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(null, { status: 401 });
+    }
+
+    return NextResponse.json(user, { status: 200 });
+  } catch {
+    return NextResponse.json(null, { status: 401 });
+  }
+}
