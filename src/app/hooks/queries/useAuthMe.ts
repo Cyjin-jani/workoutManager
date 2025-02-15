791bab36a1af639831fefad32373fca44984c1dd
Cyjin-jani <jinjanic91@gmail.com>
2025-02-15 16:07:49 +0900

    feat: process에서 env 접근하는 대신 t3env로 만든 스키마에서 접근하도록 수정

--- src/app/hooks/queries/useAuthMe.ts
+++ src/app/hooks/queries/useAuthMe.ts
@@ -1,8 +1,9 @@
+import { env } from '@/env';
 import type { User } from '@prisma/client';
 import { type UseSuspenseQueryResult, queryOptions, useSuspenseQuery } from '@tanstack/react-query';
 
 const isServer = typeof window === 'undefined';
-const BASE_URL = isServer ? process.env.API_BASE_URL : process.env.NEXT_PUBLIC_API_BASE_URL;
+const BASE_URL = env.NEXT_PUBLIC_API_BASE_URL;
 
 export const fetchAuthMe = async (): Promise<User> => {
   const headers = {
