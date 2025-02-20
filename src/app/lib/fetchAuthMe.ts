93c1db8c627d42f1f548a0af1306db53a4e2e15b
Cyjin-jani <jinjanic91@gmail.com>
2025-02-20 20:59:13 +0900

    fix: build 에러 수정 (throw error 방식 변경)

--- src/app/lib/fetchAuthMe.ts
+++ src/app/lib/fetchAuthMe.ts
@@ -19,6 +19,7 @@ export const fetchAuthMe = async (): Promise<User> => {
     const data = (await response.json()) as User;
     return data;
   } catch (error) {
-    throw new Error('Something went wrong!');
+    console.error('Failed to fetch auth data:', error);
+    throw error;
   }
 };
