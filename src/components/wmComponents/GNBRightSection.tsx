0ceffcfab787adfcbfbe08396898c1a23070c9fe
Cyjin-jani <jinjanic91@gmail.com>
2025-02-21 00:29:12 +0900

    feat: 로그아웃 기능 적용

--- src/components/wmComponents/GNBRightSection.tsx
+++ src/components/wmComponents/GNBRightSection.tsx
@@ -1,15 +1,34 @@
 import { useAuthMe } from '@/app/hooks/queries/useAuthMe';
+import { useLogout } from '@/app/hooks/useLogout';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { MyAvatar } from '@/components/wmComponents/Avatar';
 import { MyButton } from '@/components/wmComponents/Button';
 import Link from 'next/link';
 
 export const GNBRightSection = () => {
   const { data: user } = useAuthMe();
+  const { logout } = useLogout();
 
   return (
     <div className="flex items-center gap-4">
       {user ? (
-        <MyAvatar profileUrl={user.profileUrl} displayName={user.name} />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MyAvatar profileUrl={user.profileUrl} displayName={user.name} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Hi, {user.name}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={logout}>로그아웃</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       ) : (
         <MyButton asChild>
           <Link href="/login" className="text-sm">
