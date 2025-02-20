54f8d851ddef7b9ed2f0dba498215aa7913d0aaf
Cyjin-jani <jinjanic91@gmail.com>
2025-02-21 00:28:01 +0900

    feat: 로그아웃 api 추가

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,14 @@
+import { AUTH_ACCESS_TOKEN } from '@/app/constants/auth';
+import { NextResponse } from 'next/server';
+
+export const runtime = 'edge';
+
+export async function POST() {
+  const response = NextResponse.json({ message: '로그아웃 성공' }, { status: 200 });
+  response.cookies.set(AUTH_ACCESS_TOKEN, '', {
+    expires: new Date(0),
+    path: '/',
+  });
+
+  return response;
+}
