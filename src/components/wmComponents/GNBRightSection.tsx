7b09ec649ec7f5c0ea23092a09ab7bfa7512955f
Cyjin-jani <jinjanic91@gmail.com>
2025-02-23 12:57:09 +0900

    refactor: mutate 함수로 로그아웃 하도록 수정

--- src/components/wmComponents/GNBRightSection.tsx
+++ src/components/wmComponents/GNBRightSection.tsx
@@ -1,5 +1,5 @@
 import { useAuthMe } from '@/app/hooks/queries/useAuthMe';
-import { useLogout } from '@/app/hooks/useLogout';
+import { useLogout } from '@/app/hooks/queries/useLogout';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,33 +9,27 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { MyAvatar } from '@/components/wmComponents/Avatar';
-import { MyButton } from '@/components/wmComponents/Button';
-import Link from 'next/link';
 
 export const GNBRightSection = () => {
   const { data: user } = useAuthMe();
-  const { logout } = useLogout();
+  const { mutate: logout } = useLogout();
+
+  const handleLogout = () => {
+    logout();
+  };
 
   return (
     <div className="flex items-center gap-4">
-      {user ? (
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <MyAvatar profileUrl={user.profileUrl} displayName={user.name} />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>Hi, {user.name}</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={logout}>로그아웃</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        <MyButton asChild>
-          <Link href="/login" className="text-sm">
-            로그인
-          </Link>
-        </MyButton>
-      )}
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MyAvatar profileUrl={user.profileUrl} displayName={user.name} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Hi, {user.name}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleLogout}>로그아웃</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
