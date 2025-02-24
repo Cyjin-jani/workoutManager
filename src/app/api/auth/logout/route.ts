fa52101070c54241249f7a68264b65ced71039bb
Cyjin-jani <jinjanic91@gmail.com>
2025-02-24 23:56:47 +0900

    refactor: next/headers의 쿠키를 이용하여 제거하도록 수정

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,11 +1,13 @@
 import { AUTH_ACCESS_TOKEN_KEY } from '@/app/constants/auth';
+import { cookies } from 'next/headers';
 import { type NextRequest, NextResponse } from 'next/server';
 
 export const runtime = 'edge';
 
 export async function POST(request: NextRequest) {
-  const response = NextResponse.redirect(new URL('/', request.url));
-  response.cookies.delete(AUTH_ACCESS_TOKEN_KEY);
+  const cookieStore = await cookies();
+  cookieStore.delete(AUTH_ACCESS_TOKEN_KEY);
 
+  const response = NextResponse.redirect(new URL('/', request.url));
   return response;
 }
